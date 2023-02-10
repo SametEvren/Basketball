15d7430671e0d0ac2aad436bfc9e0f3778b5d8a1
Samet <sametevren9@gmail.com>
2023-02-11 01:46:23 +0300

    feat: Shoot mechanic changed(unpolished)

--- Assets/Scripts/CharacterBallInteraction.cs
+++ Assets/Scripts/CharacterBallInteraction.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using Random = UnityEngine.Random;
 
 public class CharacterBallInteraction : MonoBehaviour
 {
@@ -12,6 +14,8 @@ public class CharacterBallInteraction : MonoBehaviour
     public Transform hoopPos;
     public int shotPower;
     public int shotHeight;
+    public Transform aboveRim;
+    public float shootingAbility;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag($"Ball"))
@@ -50,10 +54,31 @@ public class CharacterBallInteraction : MonoBehaviour
         StartCoroutine(OpenCollider());
         ball.transform.parent = null;
         ball.isKinematic = false;
-        Debug.Log("Distance: "+ Vector3.Distance(transform.position,hoopPos.position));
-        Vector3 hoopNewPos = new Vector3(hoopPos.position.x, shotHeight, hoopPos.position.z);
-        Vector3 direction = (hoopNewPos - transform.position).normalized;
-        ball.AddForce(direction * shotPower);
+        //Debug.Log("Distance: "+ Vector3.Distance(transform.position,hoopPos.position));
+
+        // float distance = Vector3.Distance(transform.position, hoopPos.position);
+        // if (distance >= 17)
+        //     shotPower = 700;
+        // if (distance >= 8 && distance < 17)
+        //     shotPower = 600;
+        // if (distance >= 5 && distance < 8)
+        //     shotPower = 500;
+        // if (distance >= 0 && distance < 5)
+        //     shotPower = 450;
+        //
+        // Vector3 hoopNewPos = new Vector3(aboveRim.position.x, shotHeight, aboveRim.position.z);
+        // Vector3 direction = (hoopNewPos - transform.position).normalized;
+        // ball.AddForce(direction * shotPower);
+        
+        // Vector3[] path = { ball.transform.position, aboveRim.position };
+        // ball.transform.DOPath(path, 1f, PathType.CatmullRom);
+        ball.transform.DOMoveX(aboveRim.position.x + Random.Range(-1f,1f) / shootingAbility, 1f).SetEase(Ease.Linear);
+        ball.transform.DOMoveZ(aboveRim.position.z + Random.Range(-1f,1f) / shootingAbility, 1f).SetEase(Ease.Linear);
+        ball.transform.DOMoveY(aboveRim.position.y + 3, 0.5f).OnComplete(() =>
+        {
+            ball.transform.DOMoveY(aboveRim.position.y, 0.5f).SetEase(Ease.InSine);
+        }).SetEase(Ease.OutSine);
+        
         holdingBall = false;
     }
 }
